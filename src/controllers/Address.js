c2c0ef24008e64abb0c88ec7be66e4d22d85d1fc
Levi Martins <levi.gm99@gmail.com>
2022-03-24 09:41:29 -0300

    CRUD (Address, Invoice, Hotel) menos op de Update

---
+++ src/controllers/Address.js
@@ -0,0 +1,72 @@
+const mariadb = require('mariadb');
+
+module.exports = {
+    async getByID(req, res) {
+        const {id} = req.query;
+        try {
+            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
+            var rows = await connection.query(`SELECT * FROM thia1892_bomsono.Address WHERE address_id=${id}`);        
+        } catch(err) {
+            var rows = "failed!";
+        }
+
+        connection.destroy();
+        
+        res.json(rows);
+    },
+
+    async getAll(req, res) {
+        try {
+            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
+            var rows = await connection.query('SELECT * FROM thia1892_bomsono.Address');        
+        } catch(err) {
+            var rows = "failed!";
+        }
+
+        connection.destroy();
+        
+        res.json(rows);
+    },
+
+    async post(req, res) {
+        const {street, number, neighborhood, city, state} = req.query;
+        try {
+            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
+            var rows = await connection.query(`INSERT INTO thia1892_bomsono.Address (street, number, neighborhood, city, state) VALUES (?,?,?,?,?)`, [street, number, neighborhood, city, state]);
+        } catch(err) {
+            var rows = "failed!";
+        }
+
+        connection.destroy();
+        
+        res.json("Worked!");
+    },
+
+    /*async patch(req, res) {
+        const {id, name, price} = req.query;
+        try {
+            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
+            var rows = await connection.query(`UPDATE thia1892_bomsono.Product SET Nome_Livro = 'SSH, o Shell Seguro' WHERE ID_LIVRO = 101;`, [name, price]);
+        } catch(err) {
+            var rows = "failed!";
+        }
+
+        connection.destroy();
+        
+        res.json("Worked!");
+    },*/
+
+    async delete(req, res) {
+        const {id} = req.query;
+        try {
+            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
+            var rows = await connection.query(`DELETE FROM thia1892_bomsono.Address WHERE address_id=${id}`);        
+        } catch(err) {
+            var rows = "failed!";
+        }
+
+        connection.destroy();
+        
+        res.json("Deleted!");
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const {Router} = require('express');
 
-const products = require('./controllers/Product.js');
+const products = require('./controllers/Product');
+const invoice = require('./controllers/Invoice');
+const address = require('./controllers/Address');
+const hotel = require('./controllers/Hotel');
 
 const routes = Router();
 
@@ -12,5 +15,26 @@ routes.get('/product', products.getByID); // retorna produto específico
 routes.post('/product', products.post);
 routes.delete('/product', products.delete);
 //routes.patch('/product', products.patch); // TIRAR DÚVIDA
+
+// CRUD Invoice
+routes.get('/allinvoices', invoice.getAll); // retorna todos os produtos
+routes.get('/invoice', invoice.getByID); // retorna produto específico
+routes.post('/invoice', invoice.post);
+routes.delete('/invoice', invoice.delete);
+//routes.patch('/invoice', invoice.patch); // TIRAR DÚVIDA
+
+// CRUD Address
+routes.get('/alladdresses', address.getAll); // retorna todos os produtos
+routes.get('/address', address.getByID); // retorna produto específico
+routes.post('/address', address.post);
+routes.delete('/address', address.delete);
+//routes.patch('/address', address.patch); // TIRAR DÚVIDA
+
+// CRUD Hotel
+routes.get('/allhotels', hotel.getAll); // retorna todos os produtos
+routes.get('/hotel', hotel.getByID); // retorna produto específico
+routes.post('/hotel', hotel.post);
+routes.delete('/hotel', hotel.delete);
+//routes.patch('/hotel', hotel.patch); // TIRAR DÚVIDA
 
 module.exports = routes;
