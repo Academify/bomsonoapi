9cf9bcbe39b7d8ace99f9a0cbb7553a3425f4dbc
Levi Martins <levi.gm99@gmail.com>
2022-03-24 02:24:33 -0300

    CRUD Produtos menos UPDATE

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 const {Router} = require('express');
 
+const products = require('./controllers/Product.js');
+
 const routes = Router();
 
-routes.get('/', (req, res) => res.send("Api da rede bom sono"))
+routes.get('/', (req, res) => res.send("Api da rede bom sono"));
+
+// CRUD Products
+routes.get('/allproducts', products.getAll); // retorna todos os produtos
+routes.get('/product', products.getByID); // retorna produto específico
+routes.post('/product', products.post);
+routes.delete('/product', products.delete);
+//routes.patch('/product', products.patch); // TIRAR DÚVIDA
 
 module.exports = routes;
