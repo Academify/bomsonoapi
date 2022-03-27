e1db783d7e3ca0a4e3462185645ce7c2e95eed02
devlipe <felipe.p.ferreira@ufv.br>
2022-03-26 23:46:14 -0300

    Questão A

--- src/controllers/Booking.js
+++ src/controllers/Booking.js
@@ -1,48 +1,68 @@
-const mariadb = require('mariadb');
+const mariadb = require("mariadb");
 
 module.exports = {
-    async getByID(req, res) {
-        const {id} = req.query;
-        try {
-            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
-            var rows = await connection.query(`SELECT * FROM thia1892_bomsono.Booking WHERE booking_id=${id}`);        
-        } catch(err) {
-            var rows = "failed!";
-        }
+  async getByID(req, res) {
+    const { id } = req.query;
+    try {
+      var connection = await mariadb.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+      });
+      var rows = await connection.query(
+        `SELECT * FROM thia1892_bomsono.Booking WHERE booking_id=${id}`
+      );
+    } catch (err) {
+      var rows = "failed!";
+    }
 
-        connection.destroy();
-        
-        res.json(rows);
-    },
+    connection.destroy();
 
-    async getAll(req, res) {
-        try {
-            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
-            var rows = await connection.query('SELECT * FROM thia1892_bomsono.Booking');        
-        } catch(err) {
-            var rows = "failed!";
-        }
+    res.json(rows);
+  },
 
-        connection.destroy();
-        
-        res.json(rows);
-    },
+  async getAll(req, res) {
+    try {
+      var connection = await mariadb.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+      });
+      var rows = await connection.query(
+        "SELECT * FROM thia1892_bomsono.Booking"
+      );
+    } catch (err) {
+      var rows = "failed!";
+    }
 
-    async post(req, res) {
-        const {number_people, start_date, end_date, client, room_type} = req.body;
-        try {
-            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
-            var rows = await connection.query(`INSERT INTO thia1892_bomsono.Booking (number_people, start_date, end_date, client, room_type) VALUES (?,?,?,?,?)`, [number_people, start_date, end_date, client, room_type]);
-        } catch(err) {
-            var rows = "failed!";
-        }
+    connection.destroy();
 
-        connection.destroy();
-        
-        res.json("Worked!");
-    },
+    res.json(rows);
+  },
 
-    /*async patch(req, res) {
+  async post(req, res) {
+    const { number_people, start_date, end_date, client, booked_room_type } =
+      req.body;
+    try {
+      var connection = await mariadb.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+      });
+      var rows = await connection.query(
+        `INSERT INTO thia1892_bomsono.Booking (number_people, start_date, end_date, client, room_type) VALUES (?,?,?,?,?)`,
+        [number_people, start_date, end_date, client, booked_room_type]
+      );
+    } catch (err) {
+      var rows = "failed!";
+    }
+
+    connection.destroy();
+
+    res.json("Worked!");
+  },
+
+  /*async patch(req, res) {
         const {id, name, price} = req.query;
         try {
             var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
@@ -56,17 +76,23 @@ module.exports = {
         res.json("Worked!");
     },*/
 
-    async delete(req, res) {
-        const {id} = req.query;
-        try {
-            var connection = await mariadb.createConnection({host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD});
-            var rows = await connection.query(`DELETE FROM thia1892_bomsono.Booking WHERE booking_id=${id}`);        
-        } catch(err) {
-            var rows = "failed!";
-        }
-
-        connection.destroy();
-        
-        res.json("Deleted!");
+  async delete(req, res) {
+    const { id } = req.query;
+    try {
+      var connection = await mariadb.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+      });
+      var rows = await connection.query(
+        `DELETE FROM thia1892_bomsono.Booking WHERE booking_id=${id}`
+      );
+    } catch (err) {
+      var rows = "failed!";
     }
-}
+
+    connection.destroy();
+
+    res.json("Deleted!");
+  },
+};
